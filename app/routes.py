3586faf014a20b76bba2990625dba4bc3abff90c
Rajneesh Verma <rjnshvrm23@gmail.com>
2023-10-31 22:07:26 +0100

    adding template inheritance

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,16 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'name': 'Raj Verma'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Raj Verma'}
+    # adding dummy posts for testing without DB
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
