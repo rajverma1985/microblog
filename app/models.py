0604e6c3f5422e5a5b1d5191885419185b17a65a
Rajneesh Verma <rjnshvrm23@gmail.com>
2023-11-04 17:43:54 +0100

    added legend for user_loader, Flask login stuff

--- app/models.py
+++ app/models.py
@@ -4,6 +4,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
+# The user loader is registered with Flask-Login with the @login.user_loader decorator.
+# The id that Flask-Login passes to the function as an argument is going to be a string,
+# so databases that use numeric IDs need to convert the string to integer as you see above.
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
