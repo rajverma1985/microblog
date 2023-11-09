4bb7137e516d754b6435e97bd71fd58d184cb530
Rajneesh Verma <rjnshvrm23@gmail.com>
2023-11-09 23:46:39 +0100

    added registration form for users

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Re-type Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register Now')
 
 
 class LoginForm(FlaskForm):
