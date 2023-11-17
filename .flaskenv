27a2ec60104c3e856c17987b60bad584b954a62d
Rajneesh Verma <rjnshvrm23@gmail.com>
2023-11-17 23:23:12 +0100

    register endpoint fix

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 FLASK_APP=microblog.py
-FLASK_ENV=production
+FLASK_ENV=development
+FLASK_DEBUG=true
--- app/models.py
+++ app/models.py
@@ -13,6 +13,13 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('following_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -21,12 +28,29 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    # the below-mentioned is a relationship and NOT a table.
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.following_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.following_id == user.id).count() > 0
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
