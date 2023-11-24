cc3a72f571108e524dc090432eb59991d21fd88f
Rajneesh Verma <rjnshvrm23@gmail.com>
2023-11-25 00:18:24 +0100

    added async mail feature using Threads

--- app/email.py
+++ app/email.py
@@ -1,6 +1,12 @@
 from flask_mail import Message
 from flask import render_template
 from app import app, mail
+from threading import Thread
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 
 # More details here on flask mail docs: https://pythonhosted.org/Flask-Mail/
@@ -8,12 +14,13 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject=subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 
 # Main function that will send email to the user.
 def send_password_reset_email(user):
     token = user.get_password_reset_token()
+
     send_email("Reset you password -- Dev Blog!!",
                sender=app.config['ADMINS'],
                recipients=[user.email],
