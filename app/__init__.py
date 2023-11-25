d5a99052290e56ba79c6253fc55991d9a8fa9b5b
Rajneesh Verma <rjnshvrm23@gmail.com>
2023-11-25 23:01:29 +0100

    timezone and datetime sorted out

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 import logging
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 if not app.debug:
